fcfcaa778f450ec65704560501bc1eb247dbfaa8
“Andrew <andrewwoods88@gmail.com>
2017-10-11 19:07:20 -0400

    implemented created proccess for posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,27 @@ describe 'navigate' do
 	  	expect(page).to have_content(/Posts/)
   	end
   end
+
+  describe 'creation' do
+  	before do
+		visit new_post_path
+  	end
+  	it 'has a NEW form that can be reached' do
+  		
+  		expect(page.status_code).to eq(200)
+  	end
+
+  	it 'can be created from new form page' do
+	  	 
+
+	      fill_in 'post[date]', with: Date.today
+	      fill_in 'post[rationale]', with: "Some Rationale"
+
+	      click_on "Save"
+
+	      expect(page).to have_content("Some Rationale")
+  	end
+  end
+
+
 end
