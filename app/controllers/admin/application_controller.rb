b6dad441edb0e106e50facbf2a169b0390b1290a
“Andrew <andrewwoods88@gmail.com>
2017-12-19 14:08:06 -0500

    Admin Dashboard permissions so admin can access admin dashboard and users//guests redirected to home page - with tests

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -5,11 +5,22 @@
 # If you want to add pagination or other controller-level concerns,
 # you're free to overwrite the RESTful controller actions.
 module Admin
+
+  def self.admin_types
+    #if you need new admin types, add them into the below array
+    ['AdminUser']
+  end
+
   class ApplicationController < Administrate::ApplicationController
+    before_action :authenticate_user!
     before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      #unless the current user is type admin user, then do the block. 
+       unless Admin.admin_types.include?(current_user.try(:type))
+        flash[:alert] = "You are not authorized to access this page."
+        redirect_to(root_path)
+      end
     end
 
     # Override this value to specify the number of elements to display at a time
