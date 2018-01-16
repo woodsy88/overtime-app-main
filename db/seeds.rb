fb13d29805f159e079804f622c60309f30d903cd
“Andrew <andrewwoods88@gmail.com>
2018-01-16 14:49:45 -0500

    Updated seeds file with audit log

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
                     password_confirmation: "password",
                     first_name: "Jon",
                     last_name: "Snow",
-                    phone: "4322424")
+                    phone: "6475670072")
 
 puts "1 User created"
 
@@ -12,7 +12,7 @@ AdminUser.create(email: "admin@test.com",
                  password_confirmation: "password",
                  first_name: "Admin",
                  last_name: "User",
-                 phone: "4322424")
+                 phone: "6475670072")
 
 puts "1 Admin User created"
 
@@ -20,4 +20,8 @@ puts "1 Admin User created"
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
-puts "100 Posts have been created"
+100.times do |audit_log|
+    AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts "100 audit logs have been created"
