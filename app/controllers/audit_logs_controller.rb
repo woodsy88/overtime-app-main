6388659ee7cbd254fbf17a456b6f343d96e9e843
“Andrew <andrewwoods88@gmail.com>
2018-01-23 18:14:13 -0500

    Implemented functional buttons for users to confirm their overtime from the homepage

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -4,4 +4,14 @@ class AuditLogsController < ApplicationController
     @audit_logs = AuditLog.page(params[:page]).per(10)
     authorize @audit_logs
   end
+
+  def confirm
+    audit_log = AuditLog.find(params[:id])
+    #pundit - posts policy
+    authorize audit_log
+    #changing enum status from submitted to approved
+    audit_log.confirmed!
+    redirect_to root_path, notice: "Thank you, your confirmation
+                                    has been succesfuly made"
+  end
 end
