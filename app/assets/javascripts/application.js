475914cd139352ed8f197a5bb3916cf383f9975d
“Andrew <andrewwoods88@gmail.com>
2018-01-29 13:29:18 -0500

    Updated order of javascript tree

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require gritter
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require bootstrap-sprockets
