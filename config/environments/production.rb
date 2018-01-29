bcd6d3552f4d18520f4d9d9d0f4201088530e663
“Andrew <andrewwoods88@gmail.com>
2018-01-29 14:24:46 -0500

    Updated production.rb asset management rules to fix css bugs on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,10 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.assets.digest = true
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -88,4 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.serve_static_assets = true
 end
