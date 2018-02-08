aec455f49f6e75d3831f147cf563754262e82702
“Andrew <andrewwoods88@gmail.com>
2018-02-08 12:53:16 -0500

    Update callback for audit_logs to set confirmed date when a post enum is set to “confirmed”

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -6,7 +6,14 @@ class AuditLog < ApplicationRecord
 
   after_initialize :set_defaults
 
+  #Rails Call Back - this sets the end date on the date when the enum becomes confirmed
+  before_update :set_end_date, if: :confirmed?
+
   private
+
+  def set_end_date
+    self.end_date = Date.today
+  end
 
   def set_defaults
     #if there is a start_date, use that - if not run the code on the right. sets a default when value is nil
